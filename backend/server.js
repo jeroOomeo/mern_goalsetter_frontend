44fc1209e2ac02b9f9512739ac1033272c300b8e
Jerome <emorejpacomios@gmail.com>
2022-11-07 16:14:17 +0800

    test deploy 1

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,25 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 //serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://mern-goalsetter-app.onrender.com",
+    ],
+  })
+);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started at port ${port}`));
